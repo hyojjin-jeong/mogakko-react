4787a3398fa814b954bd5eb37942d7344e76178c
정효진 <jeonghyojin@keunhyojing.local>
2022-02-05 22:55:48 +0900

    컴포넌트 생성

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+function Hello(){
+    return (
+    <div>
+    <h1> 1. 유튜브</h1>
+    <p> 유튜브는 구글이 서비스하는 동영상 공유 플랫폼이다. </p>
+    <h1> 2. 넷플릭스</h1>
+    <p> 넷플릭스는 미국의 멀티미디어 엔터네인먼트 OTT 기업이다.</p>
+    </div>
+    );
+}
+
+export default Hello;
